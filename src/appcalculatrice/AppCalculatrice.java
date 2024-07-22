1a30a617c47c394b48538858d832b23130050f35
mkdeveloper <mkdeveloper224@gmail.com>
2024-07-22 15:00:02 +0000

    legere modification

--- src/appcalculatrice/AppCalculatrice.java
+++ src/appcalculatrice/AppCalculatrice.java
@@ -14,7 +14,9 @@ public class AppCalculatrice {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Calculatrice cal = new Calculatrice();
+        cal.setVisible(true);
     }
     
 }
