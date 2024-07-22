8d921d0aed26c15af6fa0c1918f9261816e2e34c
mkdeveloper <mkdeveloper224@gmail.com>
2024-07-22 14:01:05 +0000

    creation du depot

---
+++ src/appcalculatrice/AppCalculatrice.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package appcalculatrice;
+
+/**
+ *
+ * @author MK10
+ */
+public class AppCalculatrice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
